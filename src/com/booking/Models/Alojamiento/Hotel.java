2b21499f52970e6ae00024052255fa2fbc3c5aa3
Matias <alzugaray1997@gmail.com>
2024-12-19 17:32:02 -0300

    feature(development)-implementacion-metodos

--- src/com/booking/Models/Alojamiento/Hotel.java
+++ src/com/booking/Models/Alojamiento/Hotel.java
@@ -1,4 +1,24 @@
 package com.booking.Models.Alojamiento;
 
-public class Hotel {
+import java.util.Date;
+import java.util.List;
+
+import com.booking.Models.Habitacion.Habitacion;
+import com.booking.Models.Reserva.ReservaData;
+
+public class Hotel extends Alojamiento {
+
+    public Hotel () {
+
+    }
+
+    public Hotel(String nombre, String ciudad, String tipoAlojamiento, Double precioBase, List<Habitacion> habitaciones, Float calificacion, Date fechaInicio, Date fechaFin, List<ReservaData<?>> registroReservas) {
+        super(nombre, ciudad, tipoAlojamiento, precioBase, habitaciones, calificacion, fechaInicio, fechaFin, registroReservas);
+    }
+
+    @Override
+    public String decorarAlojamiento() {
+        return "Decorando hotel...";
+    }
+
 }
--- src/com/booking/Models/Habitacion/Habitacion.java
+++ src/com/booking/Models/Habitacion/Habitacion.java
@@ -1,4 +1,110 @@
 package com.booking.Models.Habitacion;
 
+import java.util.List;
+
 public class Habitacion {
+
+    private List<String> tipoDeHabitacion;
+    private double precio;
+    private String descripcion;
+    private boolean disponibilidad;
+    private int cantidadMenores;
+    private int cantidadAdultos;
+    private double precioBase;
+
+    public Habitacion() {
+    }
+
+    public Habitacion(List<String> tipoDeHabitacion, double precio, String descripcion, boolean disponibilidad,
+                      int cantidadMenores, int cantidadAdultos, double precioBase) {
+        this.tipoDeHabitacion = tipoDeHabitacion;
+        this.precio = precio;
+        this.descripcion = descripcion;
+        this.disponibilidad = disponibilidad;
+        this.cantidadMenores = cantidadMenores;
+        this.cantidadAdultos = cantidadAdultos;
+        this.precioBase = precioBase;
+    }
+
+
+    public void mostrarInformacion() {
+        System.out.println("Información de la Habitación:");
+        System.out.println("Tipo de Habitación: " + tipoDeHabitacion);
+        System.out.println("Descripción: " + descripcion);
+        System.out.println("Precio Base: $" + precioBase);
+        System.out.println("Precio Total: $" + precio);
+        System.out.println("Capacidad: " + cantidadAdultos + " adultos, " + cantidadMenores + " menores");
+    }
+
+    @Override
+    public String toString() {
+        return "Habitacion:\n" +
+                "  Tipo de Habitación: " + tipoDeHabitacion + "\n" +
+                "  Descripción: " + descripcion + "\n" +
+                "  Precio Base: $" + precioBase + "\n" +
+                "  Precio Total: $" + precio + "\n" +
+                "  Disponibilidad: " + (disponibilidad ? "Disponible" : "No Disponible") + "\n" +
+                "  Capacidad: " + cantidadAdultos + " adultos, " + cantidadMenores + " menores";
+    }
+
+    public void mostrarDisponibilidad() {
+        String estado = disponibilidad ? "Disponible" : "No Disponible";
+        System.out.println("Disponibilidad: " + estado);
+    }
+
+    public List<String> getTipoDeHabitacion() {
+        return tipoDeHabitacion;
+    }
+
+    public void setTipoDeHabitacion(List<String> tipoDeHabitacion) {
+        this.tipoDeHabitacion = tipoDeHabitacion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public boolean isDisponibilidad() {
+        return disponibilidad;
+    }
+
+    public void setDisponibilidad(boolean disponibilidad) {
+        this.disponibilidad = disponibilidad;
+    }
+
+    public int getCantidadMenores() {
+        return cantidadMenores;
+    }
+
+    public void setCantidadMenores(int cantidadMenores) {
+        this.cantidadMenores = cantidadMenores;
+    }
+
+    public int getCantidadAdultos() {
+        return cantidadAdultos;
+    }
+
+    public void setCantidadAdultos(int cantidadAdultos) {
+        this.cantidadAdultos = cantidadAdultos;
+    }
+
+    public double getPrecioBase() {
+        return precioBase;
+    }
+
+    public void setPrecioBase(double precioBase) {
+        this.precioBase = precioBase;
+    }
 }
